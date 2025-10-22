54071b0dd328149455f45339ef8c73ec5fad76a5
Jannis Fuchs <janifu02@gmail>
2025-10-22 15:55:43 +0200

    komplettes Redesign der Website. zu 100% Org-mode

--- src/components/LostPage.js
+++
@@ -1,8 +0,0 @@
-function LostPage(){
-    return(
-        <div className="container">
-            <iframe className="mainComponent" src="/blog/compiledOrgFiles/404Page.html" title="Startseite"></iframe>
-        </div>
-    )
-}
-export default LostPage
